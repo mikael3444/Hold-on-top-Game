57289fe3a256ac2ceffb5c7d89a9d25ae38f8a03
mikael3444 <mikaeltagima@gmail.com>
2016-05-26 19:05:58 -0300

    Versao Com os inimigos se movendo(So para frente).

---
+++ Assets/scripts/SpawnsPoints.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnsPoints : MonoBehaviour {
+
+    public GameObject enemy;
+    public float spawnTime;
+    public Transform[] spawnPoint;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    public void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoint.Length);
+
+        Instantiate(enemy, spawnPoint[spawnPointIndex].position, spawnPoint[spawnPointIndex].rotation);
+    }
+}
